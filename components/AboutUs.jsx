6837ff73dbebdb62a44a0683a19713f4f41ac947
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-08-26 11:10:25 -0400

    add bg

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -4,7 +4,13 @@ import React from "react";
 function AboutUs() {
   return (
     <div className="w-full flex items-center justify-center h-screen relative ">
-      <div className="my-6  text-center">
+      <Image
+        className=""
+        src={"/beizh-bg.png"}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute my-6 text-center">
         <p className="max-w-4xl italic text-2xl ">
           Sandro’s approach to cooking could be described as simultaneously
           elegant and rustic, comforting yet refined. He combines locally grown
